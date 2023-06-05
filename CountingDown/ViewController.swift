acefb1528676e2ab00c07f0f0a65aa510f906987
SebasMac <ionrodriguezlara@gmail.com>
2023-06-05 14:50:49 -0700

    Initialized project

---
+++ CountingDown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountingDown
+//
+//  Created by Ion Sebastian Rodriguez Lara on 05/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
